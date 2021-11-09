2795526ba55d7c14fb3af1058a433ddb35c134ef
Rainfall.NZ <60120098+RainfallNZ@users.noreply.github.com>
2021-11-09 19:49:21 +1300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RainCheckPy'
+copyright = '2021, Rainfall.NZ'
+author = 'Tim Kerr'
 
 release = '0.1'
 version = '0.1.0'
